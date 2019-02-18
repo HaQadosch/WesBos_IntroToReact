9f06c57693fae1eee97dd3c8457c9b37ef550230
HaQadosch Berraka <haqadosch@pm.me>
2019-02-18 19:55:47 +0000

    Lesson 8

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,15 @@
 import React from 'react'
 
-export class Header extends React.Component {
-  render () {
-    return (
-      <header className='top'>
-        <h1>Catch
-          <span className='ofThe'>
-            <span className='of'>Of</span>
-            <span className='the'>The</span>
-          </span>
-          Day</h1>
-        <h3 className='tagline'>
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    )
-  }
-}
+export const Header = ({ tagline }) => (
+  <header className='top'>
+    <h1>Catch
+      <span className='ofThe'>
+        <span className='of'>Of</span>
+        <span className='the'>The</span>
+      </span>
+        Day</h1>
+    <h3 className='tagline'>
+      <span>{tagline}</span>
+    </h3>
+  </header>
+)
